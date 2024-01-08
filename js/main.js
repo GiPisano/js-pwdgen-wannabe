1c60751896192c89c0cce6fdc6efdbec75f49c0b
GiPisano <pisanogiacomo9@gmail.com>
2024-01-08 16:25:27 +0100

    add javascript

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const userPassword = document.getElementById('password')
+
+// chiedo all'utente il suo nome 
+const userName = prompt('Qual è il tuo nome?');
+console.log(userName);
+
+// chiedo all'utente il suo cognome
+const userSurname = prompt('Qual è il tuo cognome?') ;
+console.log(userSurname);
+
+// chiedo all'utente il suo colore preferito
+const userFavoriteColor = prompt('Qual è il tuo colore preferito?') ;
+console.log(userFavoriteColor);
+
+// stampo la password creata
+userPassword.innerText = 'La password che abbiamo generato per te è' + ' ' + userName + userSurname + userFavoriteColor + 24;;
+
+
+
+
+
+
+
+
